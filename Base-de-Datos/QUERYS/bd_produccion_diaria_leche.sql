8cf27955c7c1fe7b1c043fe18b9686f84a2a1fa8
Gerson Estuardo Alonzo Tajiboy <galonzot@miumg.edu.gt>
2023-05-18 15:25:18 -0600

    Commit - Modales
    Se agregaron modales editar, dar baja y eliminar en produccion_diaria_leche
    Se agrego un modal de guardar en modificar_produccion_diaria_leche

--- Base-de-Datos/QUERYS/bd_produccion_diaria_leche.sql
+++ Base-de-Datos/QUERYS/bd_produccion_diaria_leche.sql
@@ -1,3 +1,1 @@
 SELECT * FROM granja_santa_maria.produccion_diaria_leche;
-
-SELECT * FROM granja_santa_maria.produccion_diaria_leche WHERE fecha_produccion_leche BETWEEN '2023-05-13' AND '2023-05-14';
