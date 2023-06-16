4d5b445579c182f760bce21b313c4bc07894b298
Gerson Estuardo Alonzo Tajiboy <galonzot@miumg.edu.gt>
2023-06-16 15:02:39 -0600

    Commit
    Commit

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -6,3 +6,5 @@ SELECT g_h.nombre_ganado_hembra, p_d_l.produccion_maniana_leche, p_d_l.produccio
 FROM produccion_diaria_leche AS p_d_l
 INNER JOIN ganado_hembra AS g_h
 ON p_d_l.id_ganado_hembra = g_h.id_ganado_hembra;
+
+SELECT * FROM dieta_ternera_ternero;
