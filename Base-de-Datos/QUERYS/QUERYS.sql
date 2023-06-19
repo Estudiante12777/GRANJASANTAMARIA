03e3605a106f0a3b2016e6e4e31334ccdc170a3d
Gerson Estuardo Alonzo Tajiboy <galonzot@miumg.edu.gt>
2023-06-19 11:58:16 -0600

    Cambios aplicados

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -1,6 +1,3 @@
 USE granja_santa_maria;
 
-SELECT * FROM detalle_historial_clinico_hembra;
-SELECT * FROM historial_clinico_macho;
-SELECT * FROm detalle_historial_clinico_macho;
-DESCRIBE detalle_historial_clinico_macho;
+SELECT * FROM producto;
