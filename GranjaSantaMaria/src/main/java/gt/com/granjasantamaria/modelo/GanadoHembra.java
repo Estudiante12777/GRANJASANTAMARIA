d88178d9554a2398ee97763ef26c1f04f0fcafc2
Gerson Estuardo Alonzo Tajiboy <gersontajiboy8@gmail.com>
2023-06-22 15:26:03 -0600

    Cambios aplicados

--- GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/modelo/GanadoHembra.java
+++ GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/modelo/GanadoHembra.java
@@ -3,15 +3,13 @@ package gt.com.granjasantamaria.modelo;
 import java.io.Serializable;
 import javax.persistence.*;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
-import javax.persistence.Id;
-
-import lombok.ToString;
-
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "ganado_hembra")
 public class GanadoHembra implements Serializable {
@@ -37,12 +35,12 @@ public class GanadoHembra implements Serializable {
     @JoinColumn(name = "id_raza_ganado")
     private RazaGanado razaGanado;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_prenies_ganado_hembra", insertable = false, updatable = false)
+    private PreniesGanadoHembra preniesGanadoHembra;
+
     @NotNull
     @Column(name = "estado_ganado_hembra", nullable = false)
     private boolean estadoGanadoHembra;
-
-    public String ToString() {
-        return getNombreGanadoHembra();
-    }
 
 }
--- GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/modelo/ProduccionDiariaLeche.java
+++ GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/modelo/ProduccionDiariaLeche.java
@@ -4,10 +4,12 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import javax.persistence.*;
 import javax.validation.constraints.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "produccion_diaria_leche")
 public class ProduccionDiariaLeche implements Serializable {
