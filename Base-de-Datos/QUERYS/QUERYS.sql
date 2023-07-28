f4eb159c3dc408ca13c18a2d92ae22d52c6f3d6a
Gerson Estuardo Alonzo Tajiboy <125421339+Estudiante12777@users.noreply.github.com>
2023-07-28 17:40:51 -0600

    Cambios modificados

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -1,6 +1,8 @@
 USE granja_santa_maria;
 
-SELECT * FROM pais;
-SELECT * FROM departamento;
-SELECT * FROM municipio;
-SELECT * FROM cliente;
+SELECT * FROM raza_ganado;
+
+DELETE FROM raza_ganado WHERE id_raza_ganado = 2;
+
+ALTER TABLE raza_ganado AUTO_INCREMENT = 1;
+
