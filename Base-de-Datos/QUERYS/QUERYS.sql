7dd272b3221d0cd97382362c929070f3269b2fc2
Gerson Estuardo Alonzo Tajiboy <125421339+Estudiante12777@users.noreply.github.com>
2023-07-29 15:40:00 -0600

    Cambios modificados

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -1,8 +1,15 @@
 USE granja_santa_maria;
 
 SELECT * FROM raza_ganado;
+SELECT * FROM tipo_ganado;
+
+
 
 DELETE FROM raza_ganado WHERE id_raza_ganado = 2;
 
 ALTER TABLE raza_ganado AUTO_INCREMENT = 1;
+
+SELECT id_tipo_ganado, estado_tipo_ganado FROM tipo_ganado WHERE estado_tipo_ganado = 0;
+
+UPDATE tipo_ganado SET estado_tipo_ganado = 1 WHERE id_tipo_ganado = 8;
 
