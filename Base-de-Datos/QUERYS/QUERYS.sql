438f37c96ddba60d40c7c5be0c291b3c7671a7c6
Gerson Estuardo Alonzo Tajiboy <125421339+Estudiante12777@users.noreply.github.com>
2023-08-02 00:03:18 -0600

    Cambios modificados

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -4,8 +4,12 @@ SELECT * FROM raza_ganado;
 SELECT * FROM tipo_ganado;
 SELECT * FROM ganado_macho;
 SELECT * FROM ganado_hembra;
-
-INSERT INTO usuario (username, password) VALUE ('', '');
-
-INSERT INTO rol (nombre, id_usuario) VALUES ('', 1);
+SELECT * FROM alimentacion_becerro;
+SELECT * FROM alimentacion_becerra;
+SELECT * FROM historial_clinico_macho;
+SELECT * FROM historial_clinico_hembra;
+SELECT * FROM detalle_historial_clinico_macho;
+SELECT * FROM detalle_historial_clinico_hembra;
+SELECT * FROM prenies_ganado_hembra;
+SELECT * FROM dieta_ternera_ternero;
 
