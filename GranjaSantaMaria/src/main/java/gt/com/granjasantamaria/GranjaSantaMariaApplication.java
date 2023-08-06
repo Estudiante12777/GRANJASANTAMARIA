dd8310361b8a48ebb4c47aa3ccd807dffaeb5cae
gerso <125421339+Estudiante12777@users.noreply.github.com>
2023-08-06 15:58:15 -0600

    UPDATE PROJECT

--- GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/GranjaSantaMariaApplication.java
+++ GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/GranjaSantaMariaApplication.java
@@ -10,20 +10,18 @@ import java.io.IOException;
 @SpringBootApplication
 public class GranjaSantaMariaApplication {
 
-    //private static final Logger logger = LoggerFactory.getLogger(GranjaSantaMariaApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(GranjaSantaMariaApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(GranjaSantaMariaApplication.class, args);
 
         // ABRIR EL NAVEGADOR CON LA URI DE LA APLICACION
-        /*
-        //try {
-        //    Runtime runtime = Runtime.getRuntime();
-        //    runtime.exec("cmd /c start http://localhost:8080");
-        //} catch (IOException e) {
-        //    logger.error("An error occurred while opening the browser.", e);
-        //}
-        */
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            runtime.exec("cmd /c start http://localhost:8080");
+        } catch (IOException e) {
+            logger.error("An error occurred while opening the browser.", e);
+        }
     }
 
 }
