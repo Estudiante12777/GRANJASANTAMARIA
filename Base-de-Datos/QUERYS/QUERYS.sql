cf4fcea5dbfbbf55453cb513ef7e09f35301c770
gerso <125421339+Estudiante12777@users.noreply.github.com>
2023-08-07 14:52:20 -0600

    UPDATE PROJECT

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -3,6 +3,7 @@ USE granja_santa_maria;
 SELECT * FROM raza_ganado;
 SELECT * FROM tipo_ganado;
 SELECT * FROM ganado_macho;
+SELECT * FROM ganado_hembra;
 
 DELETE FROM ganado_macho WHERE id_ganado_macho = 1;
 ALTER TABLE ganado_macho AUTO_INCREMENT = 1;
