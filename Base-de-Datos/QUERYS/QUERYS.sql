c16d2bb6c57f3740098a68761ba91a4828b7e0fc
gerso <125421339+Estudiante12777@users.noreply.github.com>
2023-08-08 17:44:32 -0600

    UPDATE PROJECT

--- Base-de-Datos/QUERYS/QUERYS.sql
+++ Base-de-Datos/QUERYS/QUERYS.sql
@@ -15,3 +15,11 @@ SELECT * FROM produccion_diaria_leche;
 
 SELECT g_h.nombre_ganado_hembra, p_d_l.produccion_maniana_leche, p_d_l.produccion_tarde_leche, p_d_l.total_produccion_leche, p_d_l.fecha_produccion_leche FROM produccion_diaria_leche AS p_d_l INNER JOIN ganado_hembra AS g_h ON p_d_l.id_ganado_hembra = g_h.id_ganado_hembra;
 
+/*
+    MODULO UBICACIO
+*/
+
+SELECT * FROM pais;
+SELECT * FROM departamento;
+SELECT * FROM municipio;
+
