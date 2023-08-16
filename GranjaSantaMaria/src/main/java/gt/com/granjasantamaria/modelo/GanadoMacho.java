0da75b55bf4c554404a895922ef4f91e6b16d375
gerso <125421339+Estudiante12777@users.noreply.github.com>
2023-08-16 17:48:17 -0600

    UPDATE PROJECT

--- GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/modelo/GanadoMacho.java
+++ GranjaSantaMaria/src/main/java/gt/com/granjasantamaria/modelo/GanadoMacho.java
@@ -1,13 +1,11 @@
 package gt.com.granjasantamaria.modelo;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -28,11 +26,6 @@ public class GanadoMacho implements Serializable {
     @NotNull
     @Column(name = "nombre_ganado_macho", nullable = false)
     private String nombreGanadoMacho;
-
-    @NotNull
-    @Column(name = "fecha_nacimiento", nullable = false)
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate fechaNacimiento;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_tipo_ganado")
